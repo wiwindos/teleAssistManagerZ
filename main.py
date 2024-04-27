37c2e2516243ba4117268d077d53e46c1e941dd3
wiw <zmzhenya@yandex.ru>
2024-04-27 17:09:31 +0500

    test tg

---
+++ main.py
@@ -0,0 +1,12 @@
+from telebot import TeleBot
+
+TOKEN = ""
+
+bot = TeleBot(TOKEN)
+
+@bot.message_handler(commands=["start", "help"])
+def send_welcome(message):
+    print(message)
+    bot.reply_to(message, "hi, baby")
+
+bot.infinity_polling()
